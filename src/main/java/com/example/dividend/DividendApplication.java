f161bebed75e2a2aad81985e6806b7317c5dec49
YujeongSon <yujeongs24@naver.com>
2023-01-20 01:05:37 +0900

    회사 저장 기능

--- src/main/java/com/example/dividend/DividendApplication.java
+++ src/main/java/com/example/dividend/DividendApplication.java
@@ -9,5 +9,4 @@ public class DividendApplication {
     public static void main(String[] args) {
         SpringApplication.run(DividendApplication.class, args);
     }
-
 }
---
+++ src/main/java/com/example/dividend/service/CompanyService.java
@@ -0,0 +1,53 @@
+package com.example.dividend.service;
+
+import com.example.dividend.model.Company;
+import com.example.dividend.model.ScrapedResult;
+import com.example.dividend.persist.entity.CompanyEntity;
+import com.example.dividend.persist.entity.DividendEntity;
+import com.example.dividend.persist.repository.CompanyRepository;
+import com.example.dividend.persist.repository.DividendRepository;
+import com.example.dividend.scraper.Scraper;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class CompanyService {
+
+    private final Scraper yahooFinanceScraper;
+    private final CompanyRepository companyRepository;
+    private final DividendRepository dividendRepository;
+
+
+    public Company save(String ticker) {
+        boolean exists = companyRepository.existsByTicker(ticker);
+        if (exists) {
+            throw new RuntimeException("already exists ticker -> " + ticker);
+        }
+        return storeCompanyAndDividend(ticker);
+    }
+
+    private Company storeCompanyAndDividend(String ticker) {
+        // ticker 를 기준으로 회사를 스크래핑한다.
+        Company company = yahooFinanceScraper.scrapCompanyByTicker(ticker);
+        if (ObjectUtils.isEmpty(company)) {
+            throw new RuntimeException("failed to scrap ticker -> " + ticker);
+        }
+
+        // 해당 회사가 존재할 경우 회사의 배당금 정보를 스크래핑한다.
+        ScrapedResult scrapedResult = yahooFinanceScraper.scrap(company);
+
+        // 스크래핑 결과
+        CompanyEntity companyEntity = companyRepository.save(new CompanyEntity(company));
+        List<DividendEntity> dividendEntityList = scrapedResult.getDividends().stream()
+                            .map(e -> new DividendEntity(companyEntity.getId(), e))
+                            .collect(Collectors.toList());
+
+        dividendRepository.saveAll(dividendEntityList);
+        return company;
+    }
+}
