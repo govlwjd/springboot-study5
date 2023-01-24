e1a8392cd16c73269167aaa56b9d3bebd9343f91
YujeongSon <yujeongs24@naver.com>
2023-01-24 19:56:57 +0900

    인증 기능

--- src/main/java/com/example/dividend/web/AuthController.java
+++ src/main/java/com/example/dividend/web/AuthController.java
@@ -1,7 +1,35 @@
 package com.example.dividend.web;
 
+import com.example.dividend.model.Auth;
+import com.example.dividend.security.TokenProvider;
+import com.example.dividend.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
+@RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final MemberService memberService;
+    private final TokenProvider tokenProvider;
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(@RequestBody Auth.SignUp request) {
+        var result = memberService.register(request);
+        return ResponseEntity.ok(result);
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> signin(@RequestBody Auth.SignIn request) {
+        var member = memberService.authenticate(request);
+        var token = tokenProvider.generateToken(member.getUsername(), member.getRoles());
+        return ResponseEntity.ok(token);
+    }
 }
